9009339f42d520f93ad7490c9b012a46f1f68871
ermin_islamagic <ermin@islamagic.com>
2022-11-22 09:50:30 +0100

    added install.php with todo

---
+++ install.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * @todo 
+ * make an install.php script that will require the wp-config.php and connect to it's db
+ * curl the zip with plugins and install
+ * insert all predefined values from plugins so there is no need to configure them nanually throug the WordPress dashboard
+ * install custom user with details and private counter role
+ * make instructions to install 
+ * reload script with js during installation
+ */
+
+/**
+ * @todo
+ * modify table options
+ *  $db_prefix_ options
+ * 
+ * selector opetion_name column:
+ *  ure_role_additional_options_values
+ *  uninstall_plugins
+ *  active_plugins
+ *  $db_prefix_ user_roles
+ *  $db_prefix_ backup_user_roles
+ *  ws_menu_editor
+ * 
+ * for start copy existing option values from the option_value column
+ */
